70985ebb88e6e7fec3592078ceffa30b5ba57375
Ashutosh Pandey <155779632+Pandey-A@users.noreply.github.com>
2025-01-08 01:54:25 +0530

    Revert "feature: added spiderman sprite"

--- src/scenes/LoadScene.js
+++ src/scenes/LoadScene.js
@@ -1,6 +1,5 @@
 import Phaser from 'phaser';
 import background from '../assets/background.png';
-import spiderman from '../assets/spiderman.png';
 import bird from '../assets/birdSprite.png';
 import title from '../assets/title.png';
 import playButton from '../assets/play_button.png';
@@ -25,22 +24,19 @@ export default class LoadScene extends Phaser.Scene {
   preload() {
     this.load.image('background', background);
 
-    this.load.spritesheet('logobird', spiderman, {
-      frameWidth: 52,
-      frameHeight: 49,
+    this.load.spritesheet('logobird', bird, {
+      frameWidth: 20,
+      frameHeight: 14,
       startFrame: 0,
-      endFrame: 5,
+      endFrame: 2,
     });
 
-    this.load.spritesheet('bird', spiderman, {
-      frameWidth: 52,
-      frameHeight: 49,
+    this.load.spritesheet('bird', bird, {
+      frameWidth: 20,
+      frameHeight: 14,
       startFrame: 0,
-      endFrame: 5,
-
+      endFrame: 2,
     });
-
-
 
     this.load.image('title', title);
 
--- src/scenes/MainScene.js
+++ src/scenes/MainScene.js
@@ -10,7 +10,6 @@ export default class MenuScene extends Phaser.Scene {
   }
 
   create() {
-    
     this.add.image(0, 0, 'background').setOrigin(0).setScale(2);
 
     this.wing_sound = this.sound.add('wing_sound');
@@ -19,7 +18,7 @@ export default class MenuScene extends Phaser.Scene {
 
     this.hit_sound = this.sound.add('hit_sound');
 
-    this.bird = this.physics.add.sprite(50, 100, 'bird').setScale(0.8);
+    this.bird = this.physics.add.sprite(50, 100, 'bird').setScale(2);
 
     this.bird.setGravityY(1000);
 
@@ -118,7 +117,7 @@ export default class MenuScene extends Phaser.Scene {
   }
 
   addTopTube(x, y) {
-    const topPipe = this.physics.add.sprite(x, y, 'top_tube').setScale(2, 3).setOrigin(0, 1);
+    const topPipe = this.physics.add.sprite(x, y, 'top_tube').setScale(2).setOrigin(0, 1);
 
     this.tubes.add(topPipe);
 
@@ -126,7 +125,7 @@ export default class MenuScene extends Phaser.Scene {
   }
 
   addBottomTube(x, y) {
-    const bottomPipe = this.physics.add.sprite(x, y, 'bottom_tube').setScale(2, 3).setOrigin(0, 0);
+    const bottomPipe = this.physics.add.sprite(x, y, 'bottom_tube').setScale(2).setOrigin(0, 0);
 
     this.tubes.add(bottomPipe);
 
@@ -134,7 +133,7 @@ export default class MenuScene extends Phaser.Scene {
   }
 
   addTubes() {
-    const hole = Math.floor(Math.random() * 6) + 3;
+    const hole = Math.floor(Math.random() * 5) + 1;
 
     this.addTopTube(288, hole * 50 + 20);
 
